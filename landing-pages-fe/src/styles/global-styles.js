5318a56db100551fba5ea8045d57375433d23bc4
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2023-01-10 17:52:27 -0300

    styled-components

---
+++ landing-pages-fe/src/styles/global-styles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle, css, theme } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    ${({ theme }) => css`
+      background: ${theme.colors.mainBg};
+    `} 
+
+
+}
+`;
